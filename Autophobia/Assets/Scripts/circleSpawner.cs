854cfd71624ce1afd9e6029ef14dbda029a42cd9
elaine356593 <zh.elaine3556@gmail.com>
2025-10-26 17:11:21 -0400

    Spawn circle at random petals.

--- Autophobia/Assets/Scripts/circleSpawner.cs
+++ Autophobia/Assets/Scripts/circleSpawner.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 
 public class CircleSpawner : MonoBehaviour
 {
-    public GameObject circlePrefab;
-    public Transform spawnPoint;
-    public float minX = -6f;
-    public float maxX = 6f;
-    public float minY = -3f;
-    public float maxY = 3f;
+    public GameObject circlePrefab;      // circle prefab
+    public Transform[] spawnPoints;     
+
+    private GameObject currentCircle;  
 
     void Start()
     {
@@ -16,8 +14,17 @@ public class CircleSpawner : MonoBehaviour
 
     public void SpawnCircle()
     {
-        if (spawnPoint != null) {
-            Instantiate(circlePrefab, spawnPoint.position, Quaternion.identity);
+        if (currentCircle != null)
+        {
+            Destroy(currentCircle);
         }
+
+        // randomly choose one petal
+        int index = Random.Range(0, spawnPoints.Length);
+        Transform chosenPoint = spawnPoints[index];
+
+        // generate circle at chosen position and set certain pertal as parent
+        currentCircle = Instantiate(circlePrefab, chosenPoint);
+        currentCircle.transform.localPosition = Vector3.zero; 
     }
 }

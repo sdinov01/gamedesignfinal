1c91e2910203037190b531d498256f7495745328
elaine356593 <zh.elaine3556@gmail.com>
2025-11-04 00:21:41 -0500

    Changes on circle Spawner and circle script
    Note: maybe need to change the code in the OnClick() function.

--- Autophobia/Assets/Scripts/circle.cs
+++ Autophobia/Assets/Scripts/circle.cs
@@ -5,75 +5,124 @@ public class circle : MonoBehaviour
     public Animator animator;
     public float growTime = 2f;
     private float timer = 0f;
-    private bool ready_click = true;
+    private bool ready_click = false;  
     private bool player_touch = false;
+
     public AudioSource musicSource;
-    public float spawnTime; //time to appear
-    public float targetTime; //time to hit
+    public float spawnTime; 
+    public float targetTime;
 
     public SpriteRenderer spriteRenderer;
-    public Sprite[] eyeSprites; //fully open, close, half open
+    public Sprite[] eyeSprites;
 
-    void Start(){
+    private bool hasBeatStarted = false;
+    private bool hasResult = false;  // avoid repeating hit/miss
+
+    void Start()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+            //clone animation controller
+            animator.runtimeAnimatorController = Instantiate(animator.runtimeAnimatorController);
+        }
+        animator.SetBool("beat", false);
     }
 
-    void Update(){
-        float songTime = musicSource.time; // current time in music
+    void Update()
+    {
+        float songTime = musicSource.time; 
         float delta = songTime - spawnTime;
 
-        // control eye animation
         float t = Mathf.Clamp01(delta / growTime);
         int frameCount = eyeSprites.Length;
         int frameIndex = Mathf.Clamp(Mathf.FloorToInt(t * frameCount), 0, frameCount - 1);
         spriteRenderer.sprite = eyeSprites[frameIndex];
 
-        if (Input.GetKeyDown(KeyCode.Space) && ready_click && player_touch){
+        if (Input.GetKeyDown(KeyCode.Space) && ready_click && player_touch && !hasResult)
+        {
             OnClick(delta);
         }
-        if (delta > 2.1f && ready_click){
-            ready_click = false;
-            animator.SetBool("miss", true);
-            FindObjectOfType<GameHandler>().ShowResult("Miss!"); //function in gamehandler.cs
-            Debug.Log("Miss!");
-            Destroy(gameObject);
-        }
-
     }
 
-    void OnClick(float delta) {
-        if (Mathf.Abs(delta - growTime) <= 0.5f) {
-            //GameHandler.Instance.AddScore(1f);
-            animator.SetBool("hit", true);
+    void OnClick(float delta)
+    {
+        hasResult = true;
+        ready_click = false;
+
+        if (Mathf.Abs(delta - growTime) <= 0.3f)
+        {
+            animator.SetTrigger("hit");
             FindObjectOfType<GameHandler>().ShowResult("Perfect");
             Debug.Log("Perfect");
         }
-        else if (Mathf.Abs(delta - growTime) <= 1f) {
-            //GameHandler.Instance.AddScore(0.5f);
-            animator.SetBool("hit", true);
+        else if (Mathf.Abs(delta - growTime) <= 0.6f)
+        {
+            animator.SetTrigger("hit");
             FindObjectOfType<GameHandler>().ShowResult("Good");
             Debug.Log("Good");
         }
-        else {
-            FindObjectOfType<GameHandler>().ShowResult("Too Late");
-            Debug.Log("Too Late");
+        else
+        {
+            animator.SetTrigger("miss");
+            FindObjectOfType<GameHandler>().ShowResult("Miss");
+            Debug.Log("Late");
         }
 
-        ready_click = false;
-
-        Destroy(gameObject);
+        // go back go idle state
+        StartCoroutine(ResetToIdle());
     }
-    
-    void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) 
+
+    System.Collections.IEnumerator ResetToIdle()
+    {
+        yield return new WaitForSeconds(1.0f); // wait until the animation has finished
+        animator.SetBool("beat", false);
+        hasBeatStarted = false;
+        hasResult = false;
+        ready_click = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
         {
             player_touch = true;
-            Debug.Log("Player entered range!");
+                
         }
     }
-    void OnTriggerExit2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
             player_touch = false;
             Debug.Log("Player left range!");
         }
     }
+    public void TriggerBeat()
+    {
+        if (!hasBeatStarted)
+        {
+            hasBeatStarted = true;
+            ready_click = true;
+            animator.SetBool("beat", true);
+            spawnTime = musicSource.time;
+            Debug.Log("BEAT!");
+            StartCoroutine(WaitForMiss());
+        }
+    }
+
+    private System.Collections.IEnumerator WaitForMiss()
+    {
+        yield return new WaitForSeconds(growTime + 0.5f); 
+        if (!hasResult)  //does not hit
+        {
+            animator.SetTrigger("miss");
+            FindObjectOfType<GameHandler>().ShowResult("Miss");
+            Debug.Log($"{gameObject.name} Miss triggered!");
+            hasResult = true;
+            StartCoroutine(ResetToIdle());
+        }
+    }
 }
+

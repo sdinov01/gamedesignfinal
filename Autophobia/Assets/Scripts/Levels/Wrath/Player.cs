522badca733f31c26d9633fa139570d393ee6721
elaine356593 <zh.elaine3556@gmail.com>
2025-11-17 12:10:42 -0500

    Changes on avoid mechanic

---
+++ Autophobia/Assets/Scripts/Levels/Wrath/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float Health, MaxHealth;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

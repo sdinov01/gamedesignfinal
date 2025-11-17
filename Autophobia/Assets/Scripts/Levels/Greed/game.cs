274008eba823aaae08a075020f05e886935230b4
sdinov01 <sabine.dinovo@tufts.edu>
2025-11-17 03:32:00 -0500

    start implementing changing color

--- Autophobia/Assets/Scripts/Levels/Greed/game.cs
+++ Autophobia/Assets/Scripts/Levels/Greed/game.cs
@@ -5,10 +5,17 @@ public class game : MonoBehaviour
 {
     /* Contains n many timeStamps that have a start rotation time and end rotation time. */
     private float time = 0;
+    /* Time to start rotation */
     [SerializeField] private float[] timeStamps;
+    /* The rotation */
     [SerializeField] private float[] rotations;
+    /* The area of the clock to change color */
+    [SerializeField] private int[] area;
+
+    /* The minute hand to rotate */
     [SerializeField] private GameObject minuteHand;
     private handMovement minuteHandMovement;
+    /* Next rotation to perform */
     private int currRotation = 0;
     /* Time of rotations is the same */
     public float rotationTime = 2f;
@@ -23,15 +30,24 @@ public class game : MonoBehaviour
     {
         /* Keep track of time */
         time += Time.deltaTime;
-        /* Perform rotations */
         if (currRotation < timeStamps.Length && currRotation < rotations.Length)
         {
+            /* A rotation must be performed if this is true */
             if (time >= timeStamps[currRotation])
             {
+                /* Performs rotation */
                 minuteHandMovement.PerformRotation(rotations[currRotation], rotationTime);
+                /* Starts changing color of clock areas */
+                ChangeColor(area[currRotation]);
                 /* Move on to next rotation */
                 currRotation = currRotation + 1;
             }
         }  
+    }
+
+    /* Changes the color of an area of the clock */
+    void ChangeColor(int area)
+    {
+
     }
 }

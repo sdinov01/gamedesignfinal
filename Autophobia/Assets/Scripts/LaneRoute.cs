e1a823057a4514d36fe0fa59addab7fc76172371
jbeaniedutemple <jbdutemple29@gmail.com>
2025-11-22 12:30:18 -0500

    pride changes from the week

--- Autophobia/Assets/Scripts/LaneRoute.cs
+++ Autophobia/Assets/Scripts/LaneRoute.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using System.Collections;
 
 public class LaneRoute : MonoBehaviour
 {
-    public Transform destination;
-    public float speed = 4f;
+    public Transform        destination;
+    public float            speed = 4.28f;
+    public GameObject       ballchild;
+    public int              childcount;
 
     void Update()
     {
-        if (destination == null) return;
+        childcount = transform.childCount;
+        if (childcount == 0) return;
 
-        transform.position = Vector3.MoveTowards(
-            transform.position,
+        ballchild = transform.GetChild (0).gameObject;
+        ballchild.transform.position = Vector3.MoveTowards(
+            ballchild.transform.position,
             destination.position,
             speed * Time.deltaTime
         );

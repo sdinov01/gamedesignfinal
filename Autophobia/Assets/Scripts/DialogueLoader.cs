d6dd04750e9a7ceafa126b1a164fdb1b9c2904e3
Elgin5 <elginzou@gmail.com>
2025-11-23 23:16:33 -0500

    added win dialogue

--- Autophobia/Assets/Scripts/DialogueLoader.cs
+++ Autophobia/Assets/Scripts/DialogueLoader.cs
@@ -1,22 +1,69 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class DialogueLoader : MonoBehaviour
 {
     public string levelName = "";
     public string winSceneName = "";
     public DialogueManager dm;
+    public levelTracker lt;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (dm.isDialogueFinished()) {
-            SceneManager.LoadScene(levelName);
+        if (dm != null) {
+            if (dm.isDialogueFinished()) {
+                SceneManager.LoadScene(levelName);
+            }
+        }
+
+        if (winSceneName != "") {
+            switch (winSceneName)
+            {
+                case "wrath_end_dialogue":
+                    if (lt.isWrathComplete()) {
+                        SceneManager.LoadScene(winSceneName);
+                    }
+                    break;
+                case "pride_end_dialogue":
+                    if (lt.isPrideComplete()) {
+                        SceneManager.LoadScene(winSceneName);
+                    }
+                    break;
+                case "Gluttony_end_dialogue":
+                    if (lt.isGluttonyComplete()) {
+                        SceneManager.LoadScene(winSceneName);
+                    }
+                    break;
+                case "lust_end_dialogue":
+                    if (lt.isLustComplete()) {
+                        SceneManager.LoadScene(winSceneName);
+                    }
+                    break;
+                case "sloth_end_dialogue":
+                    if (lt.isSlothComplete()) {
+                        SceneManager.LoadScene(winSceneName);
+                    }
+                    break;
+                case "greed_end_dialogue":
+                    if (lt.isGreedComplete()) {
+                        SceneManager.LoadScene(winSceneName);
+                    }
+                    break;
+                case "envy_end_dialogue":
+                    if (lt.isEnvyComplete()) {
+                        SceneManager.LoadScene(winSceneName);
+                    }
+                    break;
+            }
         }
     }
+
 }
--- Autophobia/Assets/Scripts/levelTracker.cs
+++ Autophobia/Assets/Scripts/levelTracker.cs
@@ -20,4 +20,32 @@ public class levelTracker : MonoBehaviour
         gluttonyComplete = false;
         lustComplete = false;
     }
+
+    public bool isWrathComplete () {
+        return wrathComplete;
+    }
+
+    public bool isLustComplete () {
+        return lustComplete;
+    }
+
+    public bool isGreedComplete () {
+        return greedComplete;
+    }
+
+    public bool isGluttonyComplete () {
+        return gluttonyComplete;
+    }
+
+    public bool isPrideComplete () {
+        return prideComplete;
+    }
+
+    public bool isEnvyComplete () {
+        return envyComplete;
+    }
+
+    public bool isSlothComplete () {
+        return slothComplete;
+    }
 }

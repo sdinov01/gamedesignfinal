a1cf04a5a224379f39735ba8e057f276441e7cc9
sdinov01 <sabine.dinovo@tufts.edu>
2025-11-24 17:22:25 -0500

    readjust level select img.

--- Autophobia/Assets/Scripts/Levels/Greed/game.cs
+++ Autophobia/Assets/Scripts/Levels/Greed/game.cs
@@ -1,58 +1,23 @@
 ﻿/* Handle rotations corresponding to music */
 using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class game : MonoBehaviour
 {
-    /* Contains n many timeStamps that have a start rotation time and end rotation time. */
-    private float time = 0;
-    /* Time to start rotation */
-    [SerializeField] private float[] timeStamps;
-    /* The rotation */
-    [SerializeField] private float[] rotations;
-    //[SerializeField] private float[] changeSpeed;
-
-
-    //private int currentSpeedIndex = 0;
-    //private int currentSpeed;
-    /* Colors for changing the clock slices */
-    [SerializeField] private Color[] colors;
     /* Health bar to keep track of whether the player loses */
     [SerializeField] private healthBar health;
     /* greedIntro to get starting time */
     [SerializeField] private greedIntro intro;
-
     [SerializeField] private handMovement hourMovement;
-
-    /* The minute hand to rotate */
-    [SerializeField] private GameObject minuteHand;
-    private handMovement minuteHandMovement;
-    /* Next rotation to perform */
-    private int currRotation = 0;
-    /* Time of rotations is the same */
-    private float rotationTime = 0.5f;
-    /* Default start time */
     private float startTime = 14.5f;
-    /* Offset */
-    private float offset = 0;
-
-    [SerializeField] private float[] colorDuration;
-    [SerializeField] private float[] changeDuration;
-    private int durationIndex;
-
-    private bool canPerformRotation = true;
-    private Coroutine performRotationCoroutine;
-
     [SerializeField] private AudioSource audio;
-    private float previousRotationTime;
+    [SerializeField] private Image fillAmount;
+    private Coroutine performRotationCoroutine;
 
     void Start()
     {
-        minuteHandMovement = minuteHand.GetComponent<handMovement>();
-        durationIndex = 0;
         performRotationCoroutine = StartCoroutine(hourMovement.performRotations());
-        previousRotationTime = 0;
     }
 
     // Update is called once per frame
@@ -66,37 +31,11 @@ public class game : MonoBehaviour
             StopCoroutine(performRotationCoroutine);
             StartCoroutine(hourMovement.performRotations());
         }
-
-        /* Time to change duration of the colors */
-        if (Time.timeSinceLevelLoad > changeDuration[durationIndex] && durationIndex < colorDuration.Length - 1)
+        if (fillAmount.fillAmount == 1f && Time.timeSinceLevelLoad > startTime)
         {
-            durationIndex++;
+            levelTracker.greedComplete = true;
         }
 
-
     }
 
-
-    /* When the level is complete, go to Level_Select scene */
-    private IEnumerator endGame(float time)
-    {
-        yield return new WaitForSeconds(time);
-        levelTracker.greedComplete = true;
-        Debug.Log(levelTracker.greedComplete);
-    }
-
-    private float calculateToSecond(float timeStamp)
-    {
-        /* First add startTime */
-        float second = startTime;
-        /* Then add the second offset to the time stamp in the music */
-        second += (timeStamp + offset);
-        return second;
-    }
-
-    public void CanRotate(bool can)
-    {
-        canPerformRotation = can;
-    }
-    
 }

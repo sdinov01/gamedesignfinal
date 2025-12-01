82a67a91f44b1cd5a7b71f5c62752a169464c9f0
elaine356593 <zh.elaine3556@gmail.com>
2025-11-30 19:13:55 -0500

    Light on sloth

--- Autophobia/Assets/Scripts/Levels/Sloth/CircleSpawner_new.cs
+++ Autophobia/Assets/Scripts/Levels/Sloth/CircleSpawner_new.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Rendering.Universal;
 
 public class CircleSpawner_new : MonoBehaviour
 {
     public AudioSource musicSource;
     public Circle[] eyes;  
+    public Light2D[] lights;
     public List<float> spawnTimes = new List<float>();    
     public float bpm = 90;
 
     public List<int> spawnPlace = new List<int>();
     private int index = 0; //for eye prefabs
     private int nextIndex = 0; //for spawn time
+
+    private float idleIntensity = 0.16f;
+    private float activeIntensity = 1f;
+    private float lightFadeTime = 1.09f;
 
     void Start()
     {
@@ -33,8 +39,33 @@ public class CircleSpawner_new : MonoBehaviour
                 yield return null;
             }
             eyes[spawnPlace[index]].Trigger(triggerTime); 
+            StartCoroutine(AnimateLight(lights[spawnPlace[index]]));
             index = (index + 1) % eyes.Length;
         }
+    }
+    IEnumerator AnimateLight(Light2D light)
+    {
+        //intensity 0.3 → 0.7
+        float t = 0f;
+        while (t < lightFadeTime)
+        {
+            t += Time.deltaTime;
+            light.intensity = Mathf.Lerp(idleIntensity, activeIntensity, t / lightFadeTime);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1.09f); // wait for the eye close animation done
+
+        // set back to 0.3
+        t = 0f;
+        while (t < lightFadeTime)
+        {
+            t += Time.deltaTime;
+            light.intensity = Mathf.Lerp(activeIntensity, idleIntensity, t / lightFadeTime);
+            yield return null;
+        }
+
+        light.intensity = idleIntensity;
     }
 }
     

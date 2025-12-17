59ccff8ea3fe50a7916a659efb2bf1f1e5caedb9
Elgin5 <elginzou@gmail.com>
2025-12-16 19:58:12 -0500

    added the secret button to make all levels accessible

--- Autophobia/Assets/Scripts/GameHandler.cs
+++ Autophobia/Assets/Scripts/GameHandler.cs
@@ -79,6 +79,10 @@ public class GameHandler : MonoBehaviour
         // Please also reset all static variables here, for new games!
     }
 
+    public void HandleSecretButton() {
+        levelTracker.enableAllLevels();
+    }
+
     public void QuitGame(){
             #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
